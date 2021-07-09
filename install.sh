e28a05708949f6149a9ac15dd9bc1fb4cf33f2c6
Ryan Gloff <gloffr@sunypoly.edu>
2021-07-09 15:04:16 -0400

    Initial commit

---
+++ install.sh
@@ -0,0 +1,14 @@
+# Installing git
+sudo apt install git -y
+git config --global user.email "gloffr@sunypoly.edu"
+git config --global user.name "Ryan Gloff"
+
+# Installing vim
+sudo apt install vim -y
+
+# Installing chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+rm google-chrome-stable_current_amd64.deb
+
+
